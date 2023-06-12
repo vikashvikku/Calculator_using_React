3a7757fb118a83073c0abe9ca27bee9f3c58178a
Vikash Kumar <vikkuvikash79097@gmail.com>
2023-06-12 13:51:34 +0530

    update

--- src/App.js
+++ src/App.js
@@ -1,23 +1,36 @@
-import logo from './logo.svg';
-import './App.css';
+import { useState } from "react";
+import "./App.css";
+import Calculations from "./Calculations";
 
 function App() {
+  const [val, setVal] = useState("");
+
+  const handleClear = () => {
+    setVal("");
+  };
+  const handleValue = (value) => {
+    setVal(value);
+  };
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="main">
+      <div className="container">
+        <div className="App">
+          <p className="calculator">Calculator</p>
+
+          {/* Input Field */}
+
+          <div className="field">{val}</div>
+        </div>
+
+        {/* Component*/}
+
+        <Calculations
+          val={val}
+          clearBtn={handleClear}
+          handleValue={handleValue}
+        />
+      </div>
     </div>
   );
 }
---
+++ src/Calculations.js
@@ -0,0 +1,134 @@
+import "./Calculations.css";
+
+const Calculations = ({ val, handleValue, clearBtn }) => {
+  const signs = ["+", "-", "*", "/", "%"];
+  const handleDigit = (digit) => {
+    if (
+      signs.includes(val[val.length - 1]) &&
+      (digit === "0" || signs.includes(digit))
+    ) {
+      handleValue(val);
+    } else if (val === "" && digit === "0") {
+      handleValue(val);
+    } else handleValue(val.concat(digit));
+  };
+
+  const handleEqual = () => {
+    handleValue(String(eval(val)));
+  };
+  const handleToggle = () => {
+    if (val.includes("(")) {
+      handleValue(val.concat(")"));
+    } else {
+      handleValue(val.concat("*("));
+    }
+  };
+  const handleTogglePlusMinus = () => {
+    if (val[0] === "-") {
+      handleValue(val.concat("+"));
+    } else {
+      handleValue(val.concat("-"));
+    }
+  };
+
+  return (
+    <>
+      <div className="output">
+        {/* Output Field */}
+
+        <p>{val}</p>
+      </div>
+
+      {/* Container for handling buttons */}
+
+      <div className="calc">
+        {/* Container - 1 */}
+
+        <div className="cont-1">
+          <button className="item clear" onClick={clearBtn}>
+            C
+          </button>
+          <button className="item cal" onClick={() => handleToggle()}>
+            ( )
+          </button>
+          <button className="item cal" onClick={() => handleDigit("%")}>
+            %
+          </button>
+          <button className="item cal" onClick={() => handleDigit("/")}>
+            ÷
+          </button>
+        </div>
+
+        {/* Container 2 */}
+
+        <div className="cont-2">
+          <button className="item" onClick={() => handleDigit("7")}>
+            7
+          </button>
+          <button className="item" onClick={() => handleDigit("8")}>
+            8
+          </button>
+          <button className="item" onClick={() => handleDigit("9")}>
+            9
+          </button>
+          <button className="item cal" onClick={() => handleDigit("*")}>
+            x
+          </button>
+        </div>
+
+        {/* Container 3 */}
+
+        <div className="cont-3">
+          <button className="item" onClick={() => handleDigit("4")}>
+            4
+          </button>
+          <button className="item" onClick={() => handleDigit("5")}>
+            5
+          </button>
+          <button className="item" onClick={() => handleDigit("6")}>
+            6
+          </button>
+          <button className="item cal" onClick={() => handleDigit("-")}>
+            —
+          </button>
+        </div>
+
+        {/* Container 4 */}
+
+        <div className="cont-4">
+          <button className="item" onClick={() => handleDigit("1")}>
+            1
+          </button>
+          <button className="item" onClick={() => handleDigit("2")}>
+            2
+          </button>
+          <button className="item" onClick={() => handleDigit("3")}>
+            3
+          </button>
+          <button className="item cal" onClick={() => handleDigit("+")}>
+            +
+          </button>
+        </div>
+
+        {/* Container 5 */}
+
+        <div className="cont-5">
+          <button className="item" onClick={() => handleTogglePlusMinus()}>
+            +/-
+          </button>
+          <button className="item" onClick={() => handleDigit("0")}>
+            0
+          </button>
+          <button className="item" onClick={() => handleDigit(".")}>
+            .
+          </button>
+          <button className="item cal equal" onClick={handleEqual}>
+            =
+          </button>
+        </div>
+      </div>
+    </>
+  );
+};
+
+export default Calculations;
